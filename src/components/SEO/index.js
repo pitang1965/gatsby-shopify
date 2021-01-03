3de0df75a1ca8e737332263dc23ffdad1dd9928f
Shoji Makino <pitang1965@gmail.com>
2021-01-03 20:39:39 +0900

    Change twitter:card to summary_large_image

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -45,7 +45,11 @@ export function SEO({ description, lang, meta = [], title }) {
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: `summary_large_image`,
+        },
+        {
+          name: `twitter:creator`,
+          content: site.siteMetadata.author,
         },
         {
           name: `twitter:creator`,
