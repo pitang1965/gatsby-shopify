b8d16d1cc5b7d15be74d966dd1b08490dd9ba3f2
Shoji Makino <pitang1965@gmail.com>
2021-02-01 20:13:28 +0900

    Add overflow-x to Layout to hide horizontal scrollbar

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -7,6 +7,7 @@ export const LayoutWrapper = styled.div`
   position: relative; // 相対位置
   box-sizing: border-box; // ヘッダーやフッターを含むすべての要素の高さ＝min-height:100vhになるように指定
   padding: 0 20px 60px 20px; // 3番目の値はフッターの高さ
+  overflow-x: hidden;
 
   @media (min-width: 1024px) {
     padding: 0 20px 30px 20px; // 3番目の値はフッターの高さ
